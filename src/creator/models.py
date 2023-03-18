0729c855532dffa1305c896c183e2f2aee402ac2
Alexander <alevork@gmail.com>
2023-03-18 22:06:03 +0500

    festures: Обновлены модели и др. скрипты для работы с данными создателя

---
+++ src/creator/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from base.models import TimeStampMixin
+
+
+class Creator(TimeStampMixin):
+    """
+    Модель для хранения данных о создателе
+    """
+
+    github_url = models.URLField(verbose_name="Ссылка github")
+    resume_url = models.URLField(verbose_name="Ссылка на резюме")
+    email = models.EmailField(verbose_name="Email")
+
+    class Meta:
+        verbose_name = "Информация о создателе"
+        verbose_name_plural = "Информация о создателе"
+
+    def __str__(self) -> str:
+        return f"Объект 'создатель' (id={self.pk})"
